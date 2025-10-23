6c6be0c240ad965e3fa43dfc978a37a7bbbb2748
Rubick1337 <poss1337@gmail.com>
2025-10-24 02:03:53 +0300

    feat : add presentation layer

---
+++ Week_3_Inno-PreTrainee/API/Controllers/AuthorsController.cs
@@ -0,0 +1,97 @@
+﻿using FluentValidation;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using FluentValidation.AspNetCore;
+using Week_3_Inno_PreTrainee.Application.Dto.AuthorDto;
+using Week_3_Inno_PreTrainee.Application.Interfaces;
+using Week_3_Inno_PreTrainee.Domain.Models;
+
+namespace Week_3_Inno_PreTrainee.Web.Controllers
+{
+    [ApiController]
+    [Route("api/author")]
+    public class AuthorsController : ControllerBase
+    {
+        private readonly IServiceAuthor _service;
+        private readonly IValidator<AuthorForCreationDto> _putAuthorCreationValidator;
+        private readonly IValidator<AuthorForUpdateDto> _putAuthorUpdateValidator;
+
+        public AuthorsController(IServiceAuthor service,
+            IValidator<AuthorForCreationDto> putAuthorCreationValidator,
+            IValidator<AuthorForUpdateDto> putAuthorUpdateValidator
+            )
+        {
+            _service = service;
+            _putAuthorCreationValidator = putAuthorCreationValidator;
+            _putAuthorUpdateValidator = putAuthorUpdateValidator;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Author>> GetAll()
+        {
+            var authors = _service.GetAllAuthors();
+            return Ok(authors);
+        }
+
+        [HttpGet("{id:int}")]
+        public ActionResult<Author> GetById(int id)
+        {
+            var author = _service.GetAuthorById(id);
+            if (author == null)
+            {
+                return NotFound();
+            }
+            return Ok(author);
+        }
+
+        [HttpPost]
+        public ActionResult<AuthorForCreationDto> Create([FromBody] AuthorForCreationDto authorDto)
+        {
+            var result = _putAuthorCreationValidator.Validate(authorDto);
+            if (!result.IsValid)
+            {
+                result.AddToModelState(ModelState);
+                return BadRequest(ModelState);
+            }
+            var author = new Author
+            {
+                Name = authorDto.Name,
+                DateOfBirth = authorDto.DateOfBirth
+            };
+
+            var created = _service.CreateAuthor(author);
+            var createdDto = new AuthorForCreationDto(created.Name, created.DateOfBirth);
+            return Ok(createdDto);
+        }
+
+        [HttpPut("{id:int}")]
+        public IActionResult Update(int id, [FromBody] AuthorForUpdateDto authorDto)
+        {
+            var result = _putAuthorUpdateValidator.Validate(authorDto);
+            if (!result.IsValid)
+            {
+                result.AddToModelState(ModelState);
+                return BadRequest(ModelState);
+            }
+            var author = new Author
+            {
+                Name = authorDto.Name,
+                DateOfBirth = authorDto.DateOfBirth
+            };
+            _service.UpdateAuthor(id, author);
+            return NoContent();
+        }
+        [HttpDelete("{id:int}")]
+        public IActionResult Delete(int id)
+        {
+            var author = _service.GetAuthorById(id);
+            if (author == null)
+            {
+                return NotFound();
+            }
+            _service.DeleteAuthorById(id);
+            return NoContent();
+        }
+    }
+}
---
+++ Week_3_Inno-PreTrainee/API/Controllers/BooksController.cs
@@ -0,0 +1,94 @@
+﻿using FluentValidation;
+using FluentValidation.AspNetCore;
+using Microsoft.AspNetCore.Mvc;
+using Week_3_Inno_PreTrainee.Application.Dto.BookDto;
+using Week_3_Inno_PreTrainee.Application.Interfaces;
+using Week_3_Inno_PreTrainee.Domain.Models;
+
+namespace Week_3_Inno_PreTrainee.Web.Controllers
+{
+    [ApiController]
+    [Route("api/book")]
+    public class BooksController : ControllerBase
+    {
+        private readonly IServiceBook _service;
+        private readonly IValidator<BookForCreationDto> _putBookCreationValidator;
+        private readonly IValidator<BookUpdateDto> _putBookUpdateValidator;
+
+        public BooksController(IServiceBook service,
+            IValidator<BookForCreationDto> putBookCreationValidator,
+            IValidator<BookUpdateDto> putBookrUpdateValidator
+            )
+        {
+            _service = service;
+            _putBookCreationValidator = putBookCreationValidator;
+            _putBookUpdateValidator = putBookrUpdateValidator;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Book>> GetAll()
+        {
+            var books = _service.GetAllBooks();
+            return Ok(books);
+        }
+        [HttpGet("{id:int}")]
+        public ActionResult<Book> GetById(int id)
+        {
+            var book = _service.GetBookById(id);
+            if (book == null)
+            {
+                return NotFound();
+            }
+            return Ok(book);
+        }
+
+        [HttpPost]
+        public ActionResult<BookForCreationDto> Create([FromBody] BookForCreationDto bookDto)
+        {
+            var result = _putBookCreationValidator.Validate(bookDto);
+            if (!result.IsValid)
+            {
+                result.AddToModelState(ModelState);
+                return BadRequest(ModelState);
+            }
+            var book = new Book
+            {
+                Title = bookDto.Title,
+                PublishedYear = bookDto.PublishedYear,
+                AuthorId = bookDto.AuthorId,
+            };
+            var created = _service.CreateBook(book);
+            return Ok(created);
+        }
+
+        [HttpPut("{id:int}")]
+        public IActionResult Update(int id, [FromBody] BookUpdateDto bookDto)
+        {
+            var result = _putBookUpdateValidator.Validate(bookDto);
+            if (!result.IsValid)
+            {
+                result.AddToModelState(ModelState);
+                return BadRequest(ModelState);
+            }
+            var book = new Book
+            {
+                Title = bookDto.Title,
+                PublishedYear = bookDto.PublishedYear,
+                AuthorId = bookDto.AuthorId,
+            };
+            _service.UpdateBook(id, book);
+            return NoContent();
+        }
+        [HttpDelete("{id:int}")]
+        public IActionResult Delete(int id)
+        {
+            var book = _service.GetBookById(id);
+            if (book == null)
+            {
+                return NotFound();
+            }
+            _service.DeleteBookById(id);
+            return NoContent();
+        }
+    }
+}
