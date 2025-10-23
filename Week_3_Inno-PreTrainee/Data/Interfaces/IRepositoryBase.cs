d24c0ad1c4604d1a42850cbacd67dbb147831aa4
Rubick1337 <poss1337@gmail.com>
2025-10-24 02:02:15 +0300

    feat: add Data layer (repositories)

---
+++ Week_3_Inno-PreTrainee/Data/Interfaces/IRepositoryBase.cs
@@ -0,0 +1,11 @@
+﻿namespace Week_3_Inno_PreTrainee.Data.Interfaces
+{
+    public interface IRepositoryBase<T> where T : class
+    {
+        IEnumerable<T> GetAll();
+        T GetById(int id);
+        T Create(T item);
+        void Update(int id, T updatedData);
+        void DeleteById(int id);
+    }
+}
---
+++ Week_3_Inno-PreTrainee/Data/Repositories/RepositoryBase.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Week_3_Inno_PreTrainee.Data.Interfaces;
+using Week_3_Inno_PreTrainee.Domain.Interfaces;
+
+namespace Week_3_Inno_PreTrainee.Infrastructure.Repositories
+{
+    public abstract class RepositoryBase<T> : IRepositoryBase<T> where T : class, IIWithId
+    {
+        protected readonly List<T> _items = new();
+
+        public IEnumerable<T> GetAll()
+        {
+            return _items.ToList();
+        }
+
+        public T GetById(int id)
+        {
+            return _items.FirstOrDefault(x => x.Id == id);
+        }
+
+        public T Create(T item)
+        {
+            item.Id = _items.Count + 1;
+            _items.Add(item);
+            return item;
+        }
+
+        public void Update(int id, T updatedData)
+        {
+            var idUpdate = _items.FindIndex(x => x.Id == id);
+            _items[idUpdate] = updatedData;
+        }
+
+        public void DeleteById(int id)
+        {
+            _items.RemoveAll(x => x.Id == id);
+        }
+    }
+}
