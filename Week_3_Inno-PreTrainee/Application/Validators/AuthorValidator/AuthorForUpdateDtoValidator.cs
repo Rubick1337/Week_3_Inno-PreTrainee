e6bbe7cf9f44fb442004afde86307c04032d1900
Rubick1337 <poss1337@gmail.com>
2025-10-24 02:03:24 +0300

    feat: add validation for models

---
+++ Week_3_Inno-PreTrainee/Application/Validators/AuthorValidator/AuthorForUpdateDtoValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+using Week_3_Inno_PreTrainee.Application.Dto.AuthorDto;
+
+namespace Week_3_Inno_PreTrainee.Application.Validators.AuthorValidator
+{
+    public class AuthorForUpdateDtoValidator : AbstractValidator<AuthorForUpdateDto>
+    {
+        public AuthorForUpdateDtoValidator()
+        {
+            RuleFor(author => author.Name)
+            .NotEmpty().WithMessage("Name обязательное поле");
+
+            RuleFor(author => author.DateOfBirth)
+            .LessThanOrEqualTo(DateTime.Now)
+            .WithMessage("Дата не может быть позже текущей.");
+        }
+    }
+}
---
+++ Week_3_Inno-PreTrainee/Application/Validators/BookValidator/BookForCreationDtoValidator.cs
@@ -0,0 +1,25 @@
+﻿using FluentValidation;
+using Week_3_Inno_PreTrainee.Application.Dto.BookDto;
+using Week_3_Inno_PreTrainee.Data.Interfaces;
+using Week_3_Inno_PreTrainee.Domain.Models;
+
+namespace Week_3_Inno_PreTrainee.Application.Validators.BookValidator
+{
+    public class BookForCreationDtoValidator : AbstractValidator<BookUpdateDto>
+    {
+        public BookForCreationDtoValidator(IRepositoryBase<Author> authors)
+        {
+            RuleFor(book => book.Title)
+            .NotEmpty().WithMessage("Title обязательное поле");
+
+            RuleFor(book => book.PublishedYear)
+            .NotEmpty().WithMessage("Год издания обязательное поля")
+            .LessThanOrEqualTo(DateTime.Now)
+            .WithMessage("Дата не может быть позже текущей.");
+
+            RuleFor(book => book.AuthorId)
+                .Must(id => authors.GetById(id) != null)
+                .WithMessage("Указанный AuthorId не существует.");
+        }
+    }
+}
