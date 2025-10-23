54f35ad976b3bcfbe70428b50734d15eeb25bede
Rubick1337 <poss1337@gmail.com>
2025-10-24 02:04:18 +0300

    feat: add dependency

--- Week_3_Inno-PreTrainee/Program.cs
+++ Week_3_Inno-PreTrainee/Program.cs
@@ -1,7 +1,22 @@
+using FluentValidation;
+using Week_3_Inno_PreTrainee.Application.Services;
+using Week_3_Inno_PreTrainee.Application.Interfaces;
+using Week_3_Inno_PreTrainee.Infrastructure.Repositories;
+using Week_3_Inno_PreTrainee.Data.Interfaces;
+using Week_3_Inno_PreTrainee.Domain.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 
 builder.Services.AddControllers();
+
+builder.Services.AddValidatorsFromAssemblyContaining<Program>();
+
+builder.Services.AddSingleton<IRepositoryBase<Author>, RepositoryAuthor>();
+builder.Services.AddSingleton<IRepositoryBase<Book>, RepositoryBook>();
+
+builder.Services.AddSingleton<IServiceAuthor, ServiceAuthor>();
+builder.Services.AddSingleton<IServiceBook, ServiceBook>();
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
