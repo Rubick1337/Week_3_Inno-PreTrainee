798b686dfffd1b0664494bbdd84b6e2a69b64c03
Rubick1337 <poss1337@gmail.com>
2025-11-01 23:39:45 +0300

    feat: add middleware for extation

---
+++ Week_3_Inno-PreTrainee/Middleware/ExceptionHandlingMiddleware.cs
@@ -0,0 +1,35 @@
+﻿namespace Week_3_Inno_PreTrainee.Middleware
+{
+    public class ExceptionHandlingMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<ExceptionHandlingMiddleware> _logger;
+
+        public ExceptionHandlingMiddleware(
+            RequestDelegate next,
+            ILogger<ExceptionHandlingMiddleware> logger)
+        {
+            _next = next;
+            _logger = logger;
+        }
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Exception:{Messege}", ex.Message);
+                await context.Response.WriteAsync(ex.Message);
+            }
+        }
+    }
+    public static class ExceptionHandlingMiddlewareExtasion
+    {
+        public static IApplicationBuilder UseException(this IApplicationBuilder app)
+        {
+            return app.UseMiddleware<ExceptionHandlingMiddleware>();
+        }
+    }
+}
