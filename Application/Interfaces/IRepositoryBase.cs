b351e81e9cd55d8a4de422bdce4a4b6ae4fa51e0
Rubick1337 <poss1337@gmail.com>
2025-11-02 21:48:13 +0300

    fix: replace interface in application

---
+++ Application/Interfaces/IRepositoryBase.cs
@@ -0,0 +1,11 @@
+﻿namespace Week_3_Inno_PreTrainee.Domain.Interfaces
+{
+    public interface IRepositoryBase<T> where T : class
+    {
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<T?> GetByIdAsync(int id);
+        Task<T> CreateAsync(T item);
+        Task UpdateAsync(int id, T updated);
+        Task DeleteByIdAsync(int id);
+    }
+}
